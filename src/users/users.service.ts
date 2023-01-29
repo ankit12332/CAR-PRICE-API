02e74c23b9ccede91d9600dc56f7463b99a2edfd
ankit12332 <ankit.panda010.ap@gmail.com>
2023-01-30 00:18:09 +0530

    created find(), findOne(id), remove, update method in users.service.ts and configure it to users.controller.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,29 @@ export class UsersService {
         const user = this.repo.create({email, password});
         return this.repo.save(user);
     }
+
+    findOne(id:number){
+        return this.repo.findOne({where:{id}})
+    }
+
+    find(email:string){
+        return this.repo.find({where:{email}}); 
+    }
+
+    async update(id: number, attrs: Partial<User>){  //attrs means attributes, Partial means an object that can have any subset of properties of the 'User'
+        const user = await this.repo.findOne({where:{id}});
+        if(!user){
+            throw new Error('User not found');
+        }
+        Object.assign(user, attrs); //if user found then take all  the properties of "attrs" to "user" by using "Object.assign"
+        return this.repo.save(user)
+    }
+
+    async remove(id: number){
+        const user = await this.repo.findOne({where:{id}})
+        if(!user){
+            throw new Error('User not found');
+        }
+        return this.repo.remove(user)
+    }
 }
