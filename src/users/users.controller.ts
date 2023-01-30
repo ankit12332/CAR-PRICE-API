dbd40bf5fc9d4d8411a132a04c8b93060831d081
ankit12332 <ankit.panda010.ap@gmail.com>
2023-01-30 16:31:07 +0530

    used @Exclude() decorator in user.entity.ts and configured @UseInterceptors(ClassSerializerInterceptor) in users.controller.ts for hiding password. (FOR INSTANT SOLUTION)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpadateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -13,6 +13,7 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(ClassSerializerInterceptor) //It's used to hide the password which we defined in user.entity.ts
     @Get('/:id')
     findUser(@Param('id') id: string){
         return this.usersService.findOne(parseInt(id));
